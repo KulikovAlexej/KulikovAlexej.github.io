e28a41d2e38edde506136fe81ef41b84073939dd
KulikovAlexej <aleksey.kulikov.96@gmail.com>
2017-11-15 00:05:29 +0300

    ready

--- js/common.js
+++ js/common.js
@@ -13,17 +13,41 @@ $(function() {
 		
 	})
 
+
+	var errors = $('#bank_details input');
+	var submitBtn = document.querySelector('form #send_data');
+
+
 	// Валидация для элементов формы
 	$('.card_number input').change(function(){
-		validate(this, /\d{4}/)
+		validate(this, /^\d{4}$/)
 	});
 
 	$('.back input').change(function(){
-		validate(this, /\d{3}/)
+		validate(this, /^\d{3}$/)
 	});
 
 	$('#card_owner').change(function(){
-		validate(this, /\b[a-zA-Z]{2,}\s[a-zA-Z]{2,}\b/)
+		validate(this, /^[a-zA-Z\s]+$/)
+	})
+
+	$('#bank_details').on("submit", function(event){
+		if(errors.length > 0){
+			$.each(errors, function(index, elem){
+				elem.className = 'invalid';
+			})
+			event.preventDefault();
+			submitBtn.className = 'disabled'
+		}
+	})
+
+// показать скрыть tooltip
+	$('.tip_btn').click(function(event){
+		event.stopPropagation();
+		$('.tip_msg').show();
+	})
+	$('body:not(".tip_btn")').click(function(event){
+		$('.tip_msg').hide();
 	})
 
 	// Как только в инпуте набирается 4 символа, то фокус переходит на следующий инпут
@@ -31,13 +55,11 @@ $(function() {
 		$item = $(this);
 		$index = $item.index();
 		$value = $item.val();
-		// console.log($index, $value.length)
 		if($value.length > 3){
 			if($index < 3){
 				$(this).next().focus();
 			}
 			else{
-				// console.log(123);
 				$item.blur()
 			}
 		}
@@ -46,9 +68,26 @@ $(function() {
 	});
 
 	function validate(elem, pattern){
-		var res = elem.value.search(pattern);
-		if (res == -1) elem.className = "invalid"; 
-		else elem.className = "valid";
+		var res = pattern.test(elem.value);
+
+		if (!res) {
+			elem.className = "invalid";
+			submitBtn.className = 'disabled'
+			if(errors.index(elem) === -1){
+				errors.push(elem)
+			}		
+		} 
+		else{
+			errors = errors.filter(function(index, element){
+				return elem !== element
+			})
+			elem.className = 'valid';
+			if(errors.length == 0){
+				submitBtn.className = 'valid'
+			}
+		}
+		
 	}
-	// alert($(window).width())
 });
+
+
